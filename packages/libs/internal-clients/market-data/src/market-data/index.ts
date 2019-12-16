42ba1f1c3994b9b3d4cef5e824ba9f87d8b6d19d
Oscar <50111907+oscarcdavid@users.noreply.github.com>
2019-12-16 04:44:20 -0400

    Market-data: Internal Client (#23)
    * Market-data: Internal Client
    
    * KTI-44 fix
    
    * Fixing @abx-types/market-data exports

---
+++ packages/libs/internal-clients/market-data/src/market-data/index.ts
@@ -0,0 +1,36 @@
+import { Transaction } from 'sequelize'
+import { getEpicurusInstance } from '@abx/db-connection-utils'
+import { OrderDirection, SymbolDepth } from '@abx-types/order'
+import { DepthMidPrice, MidPricesForSymbolRequest } from '@abx-types/market-data'
+
+import { MarketDataEndpoints } from './endpoints'
+
+export async function calculateRealTimeMidPriceForSymbol(symbolId: string): Promise<number> {
+  const epicurus = getEpicurusInstance()
+
+  return epicurus.request(MarketDataEndpoints.calculateRealTimeMidPriceForSymbol, { symbolId })
+}
+
+export async function calculateMidPriceForSymbol(
+  symbolId: string,
+  symbolDepth: SymbolDepth = { [OrderDirection.buy]: [], [OrderDirection.sell]: [] },
+  transaction?: Transaction,
+) {
+  const epicurus = getEpicurusInstance()
+
+  return epicurus.request(MarketDataEndpoints.calculateMidPriceForSymbol, { symbolId, symbolDepth, transaction })
+}
+
+export async function calculateRealTimeMidPriceForSymbols(symbolIds: string[]): Promise<Map<string, number>> {
+  const epicurus = getEpicurusInstance()
+
+  return epicurus.request(MarketDataEndpoints.calculateRealTimeMidPriceForSymbols, { symbolIds })
+}
+
+export async function getMidPricesForSymbol(request: MidPricesForSymbolRequest): Promise<DepthMidPrice[]> {
+  const epicurus = getEpicurusInstance()
+
+  return epicurus.request(MarketDataEndpoints.getMidPricesForSymbol, { request })
+}
+
+export * from './endpoints'
