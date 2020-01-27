97147238e93f173eb05dcd3d5333e69aeec4bc18
Borislav Shekerov <shekerovborislav@gmail.com>
2020-01-27 09:05:36 +0200

    Fix spelling issue on dependency import

--- packages/services/market-data/src/core/__tests__/repository/daily-statistics/retrieval/retrieve_daily_order_match_amounts_repository.spec.ts
+++ packages/services/market-data/src/core/__tests__/repository/daily-statistics/retrieval/retrieve_daily_order_match_amounts_repository.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { MemoryCache, truncateTables } from '@ABX/db-connection-utils'
+import { MemoryCache, truncateTables } from '@abx/db-connection-utils'
 import { getDailyVolume } from '../../../../repository/daily-statistics'
 
 describe('Retrieve the order match amounts', async () => {
