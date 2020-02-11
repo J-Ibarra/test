d452149b089da5f10406cefa05a2966ec39bdd71
Borislav Shekerov <shekerovborislav@gmail.com>
2020-02-11 17:13:41 +0200

    Fixing .travis.yml #2

---
+++ _scripts/travis-build-branch-checkout.sh
@@ -0,0 +1,16 @@
+# Keep track of where Travis put us.
+# We are on a detached head, and we need to be able to go back to it.
+local build_head=$(git rev-parse HEAD)
+
+# Fetch all the remote branches. Travis clones with `--depth`, which
+# implies `--single-branch`, so we need to overwrite remote.origin.fetch to
+# do that.
+git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch
+
+# create the tacking branches (just develop for now)
+git checkout develop
+
+
+# finally, go back to where we were at the beginning
+git checkout ${build_head}
