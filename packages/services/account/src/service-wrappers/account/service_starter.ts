eae568253fbf88af517da03460e36692721999ad
Borislav Shekerov <shekerovborislav@gmail.com>
2020-02-17 13:32:00 +0200

    Fixing account image build

--- packages/services/account/src/service-wrappers/account/service_starter.ts
+++ packages/services/account/src/service-wrappers/account/service_starter.ts
@@ -3,7 +3,10 @@ import { bootstrapInternalApi } from './internal-api'
 import { ACCOUNT_REST_API_PORT } from '@abx-service-clients/account'
 
 export async function bootstrapAccountsService() {
+  console.log(`Bootstrapping account service API`)
   const publicApi = await bootstrapRestApi()
+
+  console.log(`Adding internal API endpoints`)
   await bootstrapInternalApi(publicApi)
 
   return publicApi.listen(ACCOUNT_REST_API_PORT)
