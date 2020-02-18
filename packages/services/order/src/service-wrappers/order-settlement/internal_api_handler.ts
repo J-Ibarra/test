005fa54144678c1e35544b1bb921dfb363554bd2
Borislav Shekerov <shekerovborislav@gmail.com>
2020-02-18 12:51:58 +0200

    Fixing order order service logging configuration #4

--- packages/services/order/src/service-wrappers/order-settlement/internal_api_handler.ts
+++ packages/services/order/src/service-wrappers/order-settlement/internal_api_handler.ts
@@ -4,16 +4,20 @@ import { mw as requestIpMiddleware } from 'request-ip'
 import { healthcheckMiddleware } from '@abx-utils/express-middleware'
 import { setupInternalApi } from '@abx-utils/internal-api-tools'
 import { SettlementEndpoints, SETTLEMENT_API_ROOT } from '@abx-service-clients/order'
-import { addOrderToSettleQueue, runSettlementLogic } from './core'
+import { addOrderToSettleQueue, runSettlementLogic, settleOrderMatchForPair } from './core'
 import { Environment } from '@abx-types/reference-data'
+import { getAllSymbolPairSummaries } from '@abx-service-clients/reference-data'
 
-export function bootstrapInternalApi() {
+export async function bootstrapInternalApi() {
   const app = express()
 
   app.use(requestIpMiddleware())
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
   app.use(healthcheckMiddleware)
+
+  const symbols = await getAllSymbolPairSummaries()
+  symbols.forEach(({ id }) => setTimeout(() => settleOrderMatchForPair(id), 100))
 
   if (process.env.NODE_ENV !== Environment.test) {
     setupInternalApi(app, [
