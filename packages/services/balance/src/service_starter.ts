6046b2993f81cb51fd5637ac44e3654209eff308
Borislav Shekerov <shekerovborislav@gmail.com>
2020-02-18 12:03:57 +0200

    Fixing balance service  logging configuration

--- packages/services/balance/src/service_starter.ts
+++ packages/services/balance/src/service_starter.ts
@@ -4,8 +4,11 @@ import { runBalanceMigrations } from './migrations/migration-runner'
 
 import './core'
 import { BALANCE_REST_API_PORT } from '@abx-service-clients/balance'
+import { Logger, LogLevel } from '@abx-utils/logging'
 
 export async function bootstrapBalanceService() {
+  Logger.configure((process.env.LOG_LEVEL as LogLevel) || LogLevel.debug)
+
   await runBalanceMigrations()
   const restApi = bootstrapRestApi()
 
