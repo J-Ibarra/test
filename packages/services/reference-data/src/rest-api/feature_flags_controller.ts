132059c58a6fc9d046a62a988164e258afa0c31c
Borislav Shekerov <shekerovborislav@gmail.com>
2020-02-18 11:12:35 +0200

    Testing logging in reference-data

--- packages/services/reference-data/src/rest-api/feature_flags_controller.ts
+++ packages/services/reference-data/src/rest-api/feature_flags_controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get, Route, Security } from 'tsoa'
 import { getFeatureFlags } from '../core'
+import { Logger } from '@abx-utils/logging'
 
 @Route('/feature-flags')
 export class FeatureFlagsController extends Controller {
+  private logger = Logger.getInstance('reference-data', '')
+
   @Security('cookieAuth')
   @Security('tokenAuth')
   @Get()
   public async retrieveAllFeatureFlags() {
+    this.logger.debug(`Retrieving feature flags`)
+    this.logger.info(`Retrieving feature flags`)
+
     return getFeatureFlags()
   }
 }
