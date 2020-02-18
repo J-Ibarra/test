7ea94382f05a223f26e8a9c6d9afba0eb33edc96
Borislav Shekerov <shekerovborislav@gmail.com>
2020-02-18 11:51:21 +0200

    Fixing market-data service  logging configuration

--- packages/services/market-data/src/service_starter.ts
+++ packages/services/market-data/src/service_starter.ts
@@ -4,8 +4,11 @@ import { runReferenceDataMigrations } from './migrations/migration-runner'
 import { Environment } from '@abx-types/reference-data'
 import { openSocket } from './websocket-api/depth-update.sockets'
 import { MARKET_DATA_REST_API_PORT } from '@abx-service-clients/market-data'
+import { Logger, LogLevel } from '@abx-utils/logging'
 
 export async function bootstrapMarketDataService() {
+  Logger.configure((process.env.LOG_LEVEL as LogLevel) || LogLevel.debug)
+
   await runReferenceDataMigrations()
   const app = bootstrapRestApi()
 
