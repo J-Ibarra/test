ed1895c91cb36b206207b10038d7b6af2bb27f7f
Borislav Shekerov <shekerovborislav@gmail.com>
2020-02-20 19:32:15 +0200

    Reports startup refactor

--- packages/services/reports/src/internal-api/index.ts
+++ packages/services/reports/src/internal-api/index.ts
@@ -1,14 +1,11 @@
 import jsreport from 'jsreport'
 
-// import { Environment, localAndTestEnvironments } from '@abx-types/reference-data'
 import { Logger } from '@abx-utils/logging'
 import { Express } from 'express'
 import { createInternalApi } from './internal_api_endpoints_container'
 import { setupInternalApi } from '@abx-utils/internal-api-tools'
 
 const logger = Logger.getInstance('reports', 'bootstrap')
-
-// let reportsServer
 
 export async function bootstrapInternalApi(publicApiExpress: Express) {
   jsreport.renderDefaults = Object.assign(jsreport.renderDefaults, {
@@ -21,7 +18,6 @@ export async function bootstrapInternalApi(publicApiExpress: Express) {
     },
   })
 
-  // if (!localAndTestEnvironments.includes(process.env.NODE_ENV as Environment) && !reportsServer) {
   logger.info('Boostrapping reports server')
   await jsreport({
     httpPort: 7777,
@@ -30,7 +26,6 @@ export async function bootstrapInternalApi(publicApiExpress: Express) {
       error: { transport: 'console', level: 'error' },
     },
   }).init()
-  // }
 
   const internalApiEndpoints = createInternalApi()
   setupInternalApi(publicApiExpress, internalApiEndpoints)
