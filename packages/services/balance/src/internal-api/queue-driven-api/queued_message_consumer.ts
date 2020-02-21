197db5e306385880d88a08240798bf4f9ea7fa53
Borislav Shekerov <shekerovborislav@gmail.com>
2020-02-21 10:00:56 +0200

    Catching eventual errors in consumeQueueMessage

--- packages/services/balance/src/internal-api/queue-driven-api/queued_message_consumer.ts
+++ packages/services/balance/src/internal-api/queue-driven-api/queued_message_consumer.ts
@@ -6,19 +6,23 @@ import { Logger } from '@abx-utils/logging'
 const logger = Logger.getInstance('balance', 'queued_message_consumer')
 const balanceMovementFacade = BalanceMovementFacade.getInstance()
 
-export function consumeQueueMessage(balanceChangeRequest: BalanceChangeAsyncRequestContainer) {
+export async function consumeQueueMessage(balanceChangeRequest: BalanceChangeAsyncRequestContainer) {
   logger.debug(`Consuming message ${JSON.stringify(balanceChangeRequest)}`)
 
-  return wrapInTransaction(sequelize, null, async tran => {
-    await Promise.all(
-      balanceChangeRequest.requestedChanges.map(change => {
-        const targetBalanceMovementFn = balanceMovementFacade[change.type].bind(balanceMovementFacade)
+  try {
+    await wrapInTransaction(sequelize, null, async tran => {
+      await Promise.all(
+        balanceChangeRequest.requestedChanges.map(change => {
+          const targetBalanceMovementFn = balanceMovementFacade[change.type].bind(balanceMovementFacade)
 
-        return targetBalanceMovementFn({
-          ...change.payload,
-          t: tran,
-        })
-      }),
-    )
-  })
+          return targetBalanceMovementFn({
+            ...change.payload,
+            t: tran,
+          })
+        }),
+      )
+    })
+  } catch (e) {
+    logger.error(`Error encountered while consuming balance change request: ${JSON.stringify(e)}`)
+  }
 }
