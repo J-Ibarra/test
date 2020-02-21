92ef9273773363846fa062629be27ccdd2b8dbaa
Borislav Shekerov <shekerovborislav@gmail.com>
2020-02-21 09:39:09 +0200

    Balance queue message consumer adding logging

--- packages/services/balance/src/internal-api/queue-driven-api/queued_message_consumer.ts
+++ packages/services/balance/src/internal-api/queue-driven-api/queued_message_consumer.ts
@@ -1,13 +1,17 @@
 import { BalanceChangeAsyncRequestContainer } from '@abx-service-clients/balance'
 import { BalanceMovementFacade } from '../../core/balance_movement_facade'
 import { sequelize, wrapInTransaction } from '@abx-utils/db-connection-utils'
+import { Logger } from '@abx-utils/logging'
 
+const logger = Logger.getInstance('balance', 'queued_message_consumer')
 const balanceMovementFacade = BalanceMovementFacade.getInstance()
 
-export function consumeQueueMessage({ requestedChanges }: BalanceChangeAsyncRequestContainer) {
+export function consumeQueueMessage(balanceChangeRequest: BalanceChangeAsyncRequestContainer) {
+  logger.debug(`Consuming message ${JSON.stringify(balanceChangeRequest)}`)
+
   return wrapInTransaction(sequelize, null, async tran => {
     await Promise.all(
-      requestedChanges.map(change => {
+      balanceChangeRequest.requestedChanges.map(change => {
         const targetBalanceMovementFn = balanceMovementFacade[change.type].bind(balanceMovementFacade)
 
         return targetBalanceMovementFn({
