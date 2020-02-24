a5f3ef973d43cdba3f96077239cf814e625dbf2b
MilesNorton <miles.norton1@hotmail.com>
2020-02-24 03:53:41 +0000

    fixing build issues, missing files

--- packages/services/deposit/src/service-wrappers/deposit-entry-processor/service_starter.ts
+++ packages/services/deposit/src/service-wrappers/deposit-entry-processor/service_starter.ts
@@ -1,11 +1,13 @@
 import '../../core'
 import { bootstrapRestApi } from './rest-api'
 import { DEPOSIT_API_PORT } from '@abx-service-clients/deposit'
-import { bootstrapInternalApi } from './internal-api'
+import { bootstrapInternalApi, bootstrapQueueDrivenApi } from './internal-api'
 import { runDepositDataMigrations } from '../../migrations/migration-runner'
 
 export async function bootstrapDepositEntryProcessor() {
   await runDepositDataMigrations()
+  await bootstrapQueueDrivenApi()
+
   const publicApi = await bootstrapRestApi()
   bootstrapInternalApi(publicApi)
 
