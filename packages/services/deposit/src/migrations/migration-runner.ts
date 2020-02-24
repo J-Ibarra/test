ed514021bff2e3d1c0527f31ef067c49291e5c40
MilesNorton <miles.norton1@hotmail.com>
2020-02-24 05:04:11 +0000

    fixed migrations

--- packages/services/deposit/src/migrations/migration-runner.ts
+++ packages/services/deposit/src/migrations/migration-runner.ts
@@ -1,8 +1,5 @@
 import { runMigrations } from '@abx-utils/db-connection-utils'
-import { runLegacyMigrations } from '@abx-utils/kbe-legacy-migrations'
 
 export function runDepositDataMigrations() {
-  return runLegacyMigrations()
-    .then(() => runMigrations(`${__dirname}/templates`))
-    .then(() => console.log('Reference data migrations successfully executed'))
+  return runMigrations(`${__dirname}/templates`).then(() => console.log('Reference data migrations successfully executed'))
 }
