740d865747a40734e64ee537d2acfc67cb432be0
Borislav Shekerov <shekerovborislav@gmail.com>
2020-02-26 19:11:53 +0200

    Fixing createWalletAddressesForNewAccount for SQS fifo

--- packages/libs/internal-clients/deposit/src/async_endpoints_handler.ts
+++ packages/libs/internal-clients/deposit/src/async_endpoints_handler.ts
@@ -4,9 +4,9 @@ import { DepositAsyncRequest } from './async_endpoint_request'
 
 export const localDepositChangeTopic = 'local-deposit-change-topic'
 
-// TODO GET RID OF THIS
 export function createWalletAddressesForNewAccount(accountId: string) {
   return sendAsyncChangeMessage<DepositAsyncRequest>({
+    id: `createWalletAddressesForNewAccount-${accountId}`
     type: DepositAsyncEndpoints.createWalletAddressesForNewAccount,
     target: {
       local: localDepositChangeTopic,
