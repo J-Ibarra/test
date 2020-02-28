19f236c8a0a8d45fa9a7971dc5b3b7d6340a7f44
Boyan Enchev <boyan.enchev@abx.com>
2020-02-28 16:16:02 +0200

    removed unused vars

--- packages/services/_local-service-starter/dev-starter-debit-card.sh
+++ packages/services/_local-service-starter/dev-starter-debit-card.sh
@@ -24,30 +24,4 @@ export KVT_INFURA_PROJECT_ID='c2ea3bc13d094d3a824322a7628f4882'
 export KVT_FEE_HOLDINGS_SECRET='B7AFD4671093505A4C1197099270F77532DF63959676824E88B91C86688C8CFA'
 export ETHERSCAN_API_DOMAIN_ROOT='ropsten'
 
-export ENV=integration
-export API_PORT=3031
-export EXCHANGE_DB_HOST=localhost
-export EXCHANGE_DB_PORT=6432
-export EXCHANGE_DB_USERNAME=postgres
-export EXCHANGE_DB_NAME=kinesis_exchange
-export EXCHANGE_DB_PASSWORD='foo'
-export DEBIT_CARD_DB_HOST=localhost
-export DEBIT_CARD_DB_PORT=5433
-export DEBIT_CARD_DB_USERNAME=postgres
-export DEBIT_CARD_DB_NAME=db
-export DEBIT_CARD_DB_PASSWORD=postgres
-export REDIS_HOST=localhost
-export REDIS_PORT=7380
-export UI_DOMAIN='localhost:1234'
-export LOG_LEVEL=debug
-export COOKIE_KEY='7yH*clwZeD0Pq&WPSYE*Q!1x9HafSs@X'
-export COOKIE_IV='67Jo*Jip5C8m6P%n'
-export JWT_SECRET=foo
-export CONTIS_USERNAME=KINEUR_beta
-export CONTIS_PASSWORD=myaqcd34
-export CONTIS_API_ROOT=https://sandboxapi.contis.com
-export CONTIS_CARD_ORDER_FEE=5
-export CONTIS_CARD_ORDER_VALIDATION_SLA=5
-
-
 npm run start
