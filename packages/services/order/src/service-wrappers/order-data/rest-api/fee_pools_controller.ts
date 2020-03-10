28dcdf7bacdf79264c2ecc96fe9779b26a51e8ff
Borislav Shekerov <shekerovborislav@gmail.com>
2020-03-10 09:46:15 +0200

    Migrating  fee pool endpoints from old repo

---
+++ packages/services/order/src/service-wrappers/order-data/rest-api/fee_pools_controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Route, Get } from 'tsoa'
+import { getApiCacheClient } from '@abx-utils/db-connection-utils'
+import { FeePool } from '@abx-types/order'
+import { findAllFeePools, findFeePool } from '../core/fee_pool_retrieval'
+
+@Route('fee-pools')
+export class FeePoolsController extends Controller {
+  @Get()
+  public async getFeePools(): Promise<FeePool[]> {
+    const cacheClient = getApiCacheClient()
+    const cacheKey = 'fee-pools'
+    const cachedResponse = await cacheClient.getCache<FeePool[]>(cacheKey)
+
+    if (cachedResponse) {
+      return cachedResponse
+    }
+
+    return findAllFeePools()
+  }
+
+  @Get('/{currencyCode}')
+  public async getFeePool(currencyCode: string): Promise<FeePool> {
+    const cacheClient = getApiCacheClient()
+    const cacheKey = `fee-pools-${currencyCode}`
+    const cachedResponse = await cacheClient.getCache<FeePool>(cacheKey)
+
+    if (cachedResponse) {
+      return cachedResponse
+    }
+
+    return findFeePool(currencyCode)
+  }
+}
