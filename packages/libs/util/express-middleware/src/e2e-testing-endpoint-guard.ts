214de63b246ac3b23631b6690daad871940ea7f6
Borislav Shekerov <shekerovborislav@gmail.com>
2020-03-12 12:53:51 +0200

    Allowing e2e endpoints in UAT temporarily

--- packages/libs/util/express-middleware/src/e2e-testing-endpoint-guard.ts
+++ packages/libs/util/express-middleware/src/e2e-testing-endpoint-guard.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Response } from 'express'
 import { OverloadedRequest } from '@abx-types/account'
-import { e2eTestingEnvironments, getEnvironment } from '@abx-types/reference-data'
+import { localAndTestEnvironments, getEnvironment, Environment } from '@abx-types/reference-data'
+
+const localAndTestEnvsWithUat = localAndTestEnvironments.concat(Environment.uat)
 
 export function e2eTestingEndpointGuard(req: OverloadedRequest | any, _res: Response, next: NextFunction) {
-  if (req.url.includes('/api/test-automation') && !e2eTestingEnvironments.includes(getEnvironment()))
-  {
-    return _res.status(400).send('Bad request');
+  if (req.url.includes('/api/test-automation') && !localAndTestEnvsWithUat.includes(getEnvironment())) {
+    return _res.status(400).send('Bad request')
   }
 
   return next()
