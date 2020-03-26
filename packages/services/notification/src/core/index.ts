745751a9140d86c6be8e889230ef9ad5b74c2466
Borislav Shekerov <shekerovborislav@gmail.com>
2020-03-26 10:29:27 +0200

    ABX-7343 fixing emails not being persisted in the DB

--- packages/services/notification/src/core/index.ts
+++ packages/services/notification/src/core/index.ts
@@ -5,4 +5,3 @@ import setupNotificationBalance from './models'
 setupModel(setupNotificationBalance)
 
 export * from './lib/email'
-export * from './lib/trade_confirmation/create_trade_confirmation_email'
