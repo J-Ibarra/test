d342dff32caef21f0d6f2b9a0988dc7a9dd9bfc2
Borislav Shekerov <shekerovborislav@gmail.com>
2020-03-31 08:33:55 +0300

    Adding keepAliveTimeout and headersTimeout to the deposit processor service api

--- packages/services/deposit/src/service-wrappers/deposit-processor/service_starter.ts
+++ packages/services/deposit/src/service-wrappers/deposit-processor/service_starter.ts
@@ -20,5 +20,9 @@ export async function bootstrapDepositProcessor() {
   const publicApi = await bootstrapRestApi()
   bootstrapInternalApi(publicApi)
 
-  return publicApi.listen(DEPOSIT_API_PORT)
+  const server = publicApi.listen(DEPOSIT_API_PORT)
+
+  /** Due to intermittent 502 response on the Load balancer side, the following 2 server settings need to be made. */
+  server.keepAliveTimeout = 65000 // Ensure all inactive connections are terminated by the ALB, by setting this a few seconds higher than the ALB idle timeout
+  server.headersTimeout = 66000 // Ensure the headersTimeout is set higher than the keepAliveTimeout due to this nodejs regression bug: https://github.com/nodejs/node/issues/27363
 }
