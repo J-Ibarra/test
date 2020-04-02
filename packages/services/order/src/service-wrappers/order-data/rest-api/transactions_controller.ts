38bd8ade595ef176f7ed6cb046ba910dd5052fcf
Borislav Shekerov <shekerovborislav@gmail.com>
2020-04-02 09:42:56 +0300

    Adding TransactionsController to order data service

---
+++ packages/services/order/src/service-wrappers/order-data/rest-api/transactions_controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Query, Request, Route, Security } from 'tsoa'
+
+import { buildAccountMonthlyTradeSummary, findTradeTransactions, findCurrencyTransactions } from '../../../core'
+import { OverloadedRequest } from '@abx-types/account'
+import { AccountMonthlyTradeSummary, TransactionType, TradeTransaction, CurrencyTransaction } from '@abx-types/order'
+
+@Route('transactions')
+export class TransactionsController extends Controller {
+  @Security('cookieAuth')
+  @Security('tokenAuth')
+  @Get('monthly-summary')
+  public async getAccountMonthlyTradeSummary(@Request() { account }: OverloadedRequest): Promise<AccountMonthlyTradeSummary> {
+    return buildAccountMonthlyTradeSummary(account!.id!)
+  }
+
+  @Security('cookieAuth')
+  @Security('tokenAuth')
+  @Get()
+  public async getTransactions(
+    @Request() request: OverloadedRequest,
+    @Query() type: TransactionType,
+    @Query() limit?: number,
+    @Query() offset?: number,
+  ): Promise<TradeTransaction[] | CurrencyTransaction[]> {
+    const query = {
+      where: {
+        accountId: request.account!.id,
+      },
+      limit,
+      offset,
+    }
+
+    const { rows } = await (type === TransactionType.trade ? findTradeTransactions(query) : findCurrencyTransactions(query))
+
+    return rows
+  }
+}
