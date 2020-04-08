a27b2186f8dc6caf5720ff5c70f21864ec093e16
Borislav Shekerov <shekerovborislav@gmail.com>
2020-04-08 21:04:36 +0300

    Build improvements

--- _scripts/travis/travis-build-feature-or-bugfix.sh
+++ _scripts/travis/travis-build-feature-or-bugfix.sh
@@ -5,4 +5,7 @@ JIRA_IMAGE_TAG="$TRAVIS_BRANCH-$TRAVIS_COMMIT"
 echo $JIRA_IMAGE_TAG
 
 # Invoking test followed by build-image-latest followed by push-image on each service package that has changed since previous commit
-lerna run test --scope $1 --since develop --include-dependencies && lerna run build-image-latest --scope $1 --since develop && lerna run push-image --scope $1 --since develop
+lerna run build  --since develop --include-dependencies \
+&& lerna run test  --since develop --include-dependencies \
+&& lerna run build-image-latest  --since develop \
+&& lerna run push-image  --since develop
--- _scripts/travis/travis-build-service-release.sh
+++ _scripts/travis/travis-build-service-release.sh
@@ -21,10 +21,6 @@ PREVIOUS_COMMIT_HASH=$(git rev-parse @~)
 echo $PREVIOUS_COMMIT_HASH 
 
 # Invoking build-image-latest script on each service package that has changed since previous commit
-lerna run build-image-latest --scope $1 --since $PREVIOUS_COMMIT_HASH
-
-# Invoking push-image script on each service package that has changed since previous commit
-lerna run push-image --scope $1 --since $PREVIOUS_COMMIT_HASH
-
-# Invoking deploy-latest-integration script on each service package that has changed since previous commit
-lerna run deploy-latest-integration --scope $1 --since $PREVIOUS_COMMIT_HASH
+lerna run build-image-latest --scope $1 --since $PREVIOUS_COMMIT_HASH \
+&& lerna run push-image --scope $1 --since $PREVIOUS_COMMIT_HASH \
+&& lerna run deploy-latest-integration --scope $1 --since $PREVIOUS_COMMIT_HASH
