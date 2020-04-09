523388c912bfe5c2917ab50558dd99ef92e5b465
Borislav Shekerov <shekerovborislav@gmail.com>
2020-04-09 18:10:05 +0300

    Minor refactor.

--- _scripts/travis/travis-build-feature-or-bugfix.sh
+++ _scripts/travis/travis-build-feature-or-bugfix.sh
@@ -4,6 +4,9 @@
 JIRA_IMAGE_TAG="$TRAVIS_BRANCH-$TRAVIS_COMMIT"
 echo $JIRA_IMAGE_TAG
 
+# Authenticating with ECR
+eval $(aws ecr get-login --region ap-southeast-2 --no-include-email)
+
 # Invoking test followed by build-image-latest followed by push-image on each service package that has changed since previous commit
 lerna run build --scope $1 --since develop --include-dependencies \
 && lerna run run-migrations:test --since develop --include-dependencies \
