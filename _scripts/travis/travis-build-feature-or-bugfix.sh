74ec4a3f7f8fd6d334fcc86835a78abc85723417
Borislav Shekerov <shekerovborislav@gmail.com>
2020-04-09 08:33:28 +0300

    Travis CI making sure migrations are executed before running tests

--- _scripts/travis/travis-build-feature-or-bugfix.sh
+++ _scripts/travis/travis-build-feature-or-bugfix.sh
@@ -6,6 +6,7 @@ echo $JIRA_IMAGE_TAG
 
 # Invoking test followed by build-image-latest followed by push-image on each service package that has changed since previous commit
 lerna run build --scope $1 --since develop --include-dependencies \
+&& lerna run run-migrations:test --since develop --include-dependencies \
 && lerna run test --scope $1 --since develop --include-dependencies \
 && lerna run build-image-latest --scope $1 --since develop \
 && lerna run push-image --scope $1 --since develop
