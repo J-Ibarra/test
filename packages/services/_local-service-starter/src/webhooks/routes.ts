dad9e8954c7b48120999b164677e6d45d0a65614
Boyan Enchev <boyan.enchev@abx.com>
2020-04-10 12:36:47 +0300

    fix webhooks payload

--- packages/services/_local-service-starter/src/webhooks/routes.ts
+++ packages/services/_local-service-starter/src/webhooks/routes.ts
@@ -9,28 +9,28 @@ export function RegisterRoutes(app: express.Express) {
     app.post('/api/webhooks/crypto/deposits/address/transactions/unconfirmed',
         function(request: any, response: any) {
             console.log(`Received deposit address transaction unconfirmed webhook`)
-            epicurus.publish(DEPOSIT_ADDRESS_UNCONFIRMED_TRANSACTION_QUEUE_URL, request.body)
+            epicurus.publish(DEPOSIT_ADDRESS_UNCONFIRMED_TRANSACTION_QUEUE_URL, { payload: request.body })
             response.status(200).end();
         });
 
     app.post('/api/webhooks/crypto/deposits/address/transactions/confirmed',
         function(request: any, response: any) {
             console.log(`Received deposit address transaction confirmed webhook`)
-            epicurus.publish(DEPOSIT_CONFIRMED_TRANSACTION_QUEUE_URL, request.body)
+            epicurus.publish(DEPOSIT_CONFIRMED_TRANSACTION_QUEUE_URL, { payload: request.body })
             response.status(200).end();
         });
 
     app.post('/api/webhooks/crypto/deposits/holdings-transactions/confirmations',
         function(request: any, response: any) {
             console.log(`Received deposit holdings transaction confirmed webhook`)
-            epicurus.publish(DEPOSIT_HOLDINGS_TRANSACTION_CONFIRMATION_QUEUE_URL, request.body)
+            epicurus.publish(DEPOSIT_HOLDINGS_TRANSACTION_CONFIRMATION_QUEUE_URL, { payload: request.body })
             response.status(200).end();
         });
 
     app.post('/api/webhooks/crypto/withdrawals/confirmations',
         function(request: any, response: any) {
             console.log(`Received withdrawal transaction confirmed webhook`)
-            epicurus.publish(WITHDRAWAL_TRANSACTION_COMPLETION_PENDING_QUEUE_URL, request.body)
+            epicurus.publish(WITHDRAWAL_TRANSACTION_COMPLETION_PENDING_QUEUE_URL, { payload: request.body })
             response.status(200).end();
         });
 }
