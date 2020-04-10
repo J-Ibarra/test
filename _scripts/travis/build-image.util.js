643fd49f899d7605f9db206d0ac5c38facbaf041
Ivan Kolev <ivan.ts.kolev@gmail.com>
2020-04-10 14:13:59 +0300

    Make spawn execution work

--- _scripts/travis/build-image.util.js
+++ _scripts/travis/build-image.util.js
@@ -1,37 +1,39 @@
 'use strict'
-const { spawn } = require('child_process');
+const {
+  spawn
+} = require('child_process')
 
-const RETRY_BUILD_TIMES = 3;
-const passedParameter = process.argv.slice(2)[0];
+const RETRY_BUILD_TIMES = 3
+const passedParameter = process.argv.slice(2)[0]
+let attempts = 1
 
-const retryImageBuild = (times = 1, commandPassed = false) => {
-    if (times > RETRY_BUILD_TIMES || commandPassed) {
-        return;
-    }
-    
-    executeBuildImageCommand();
+const retryImageBuild = (commandPassed = false) => {
+  if (commandPassed) {
+    return
+  } else if (attempts > RETRY_BUILD_TIMES) {
+    // The image build failed, this will fail the Travis build
+    process.exit(1)
+  }
+
+  executeBuildImageCommand()
 }
-
 
 const executeBuildImageCommand = () => {
-    const process = spawn(
-        'lerna run build-image-latest',
-        ['--scope', passedParameter, '--since', 'develop']
-    );
+  const process = spawn('lerna', ['run', 'build-image-latest', '--scope', passedParameter, '--since', 'develop'])
 
-    process.stdout.on('data', (data) => {
-        console.log(`lerna run build-image-latest success: ${data}`);
-    });
-    
-    process.stderr.on('data', (data) => {
-        console.error(`lerna run build-image-latest error: ${data}`);
-        retryImageBuild(++times, false);
-    });
-    
-    process.on('close', (code) => {
-        console.log(`lerna run build-image-latest exited with code ${code}`);
-        retryImageBuild(++times, code === 0);
-    });
+  process.stdout.on('data', (data) => {
+    console.log(`${data}`)
+  })
+
+  process.stderr.on('data', (data) => {
+    console.error(`${data}`)
+  })
+  
+  process.on('close', (code) => {
+    console.log(`lerna run build-image-latest exited with code ${code}`)
+    attempts++
+    retryImageBuild(code === 0)
+  })
 }
 
-retryImageBuild();
+retryImageBuild()
