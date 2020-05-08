02bc41cce06eaabce140af68d16104047c28be47
Borislav Shekerov <shekerovborislav@gmail.com>
2020-05-08 16:13:19 +0300

    Update packages/libs/model/reference-data/src/config/currency_withdrawal_config.interface.ts
    Co-authored-by: ivankollev1 <52084838+ivankollev1@users.noreply.github.com>

--- packages/libs/model/reference-data/src/config/currency_withdrawal_config.interface.ts
+++ packages/libs/model/reference-data/src/config/currency_withdrawal_config.interface.ts
@@ -6,6 +6,6 @@ export interface CurrencyWithdrawalConfig {
   minimumAmount: number
   /** The maximum fee that Kinesis is willing to pay for a for that currency transaction. */
   transactionFeeCap?: number
-  /** The increment that Kinesis want to add when calculation the fee to be paid, in order to stay ahead of the competition (priority-wise). */
+  /** The increment that Kinesis wants to add when calculating the fee to be paid, in order to stay ahead of the competition (priority-wise). */
   transactionFeeIncrement?: number
 }
